540cd84f69add53aacfc1165f12897acde3bad23
lichenxiong <2251727682@qq.com>
2022-03-24 18:09:53 +0800

    Initial commit

---
+++ src/main/java/com/mnz/wechatappserverdemo/WeChatAppServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mnz.wechatappserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeChatAppServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeChatAppServerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mnz/wechatappserverdemo/WeChatAppServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnz.wechatappserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeChatAppServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
